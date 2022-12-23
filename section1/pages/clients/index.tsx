576d174aa1b820c1166ec1dffb5d60e37a9b9fab
Beanxx <tnqls991211@naver.com>
2022-12-23 23:58:00 +0900

    feat: 다양한 Navigating 작업

--- section1/pages/clients/index.tsx
+++ section1/pages/clients/index.tsx
@@ -1,9 +1,32 @@
+import Link from "next/link";
 import styles from "../../styles/Home.module.css";
 
 export default function ClientsPage() {
+  const clients = [
+    { id: "max", name: "Maximilian" },
+    { id: "manu", name: "Manuel" },
+  ];
   return (
     <div className={styles.main}>
       <h1>The Clients Page</h1>
+      <ul>
+        <li>
+          {clients.map((client) => (
+            <li key={client.id}>
+              {/* <Link href={`/clients/${client.id}`}>{client.name}</Link> */}
+              <Link
+                href={{ pathname: "/clients/[id]", query: { id: client.id } }}
+              >
+                {client.name}
+              </Link>
+            </li>
+          ))}
+        </li>
+      </ul>
     </div>
   );
 }
+
+// Link 태그의 프로퍼티인 href를 사용하는 방법
+// 1. href={`/clients/${client.id}` 요렇게 url를 문자열로 지정
+// 2. href={{ pathname: "/clients/[id]", query: { id: client.id } }} 요렇게 동적으로 객체로 지정
--- section1/pages/index.tsx
+++ section1/pages/index.tsx
@@ -1,9 +1,21 @@
+import Link from "next/link";
 import styles from "../styles/Home.module.css";
 
 export default function HomePage() {
   return (
     <div className={styles.main}>
       <h1>The Home Page</h1>
+      <ul>
+        <li>
+          <Link href="/portfolio">Portfilo</Link>
+        </li>
+        <li>
+          <Link href="/clients">Clients</Link>
+        </li>
+      </ul>
     </div>
   );
 }
+
+// a 태그로 구현했을 때와 다르게 백엔드에게 HTTP 요청을 보낼 필요가 없어서 app state가 바뀌지 않아서 렌딩 속도가 빨라짐!
+// replace property: 새로운 페이지를 또 띄우지 않고, 현재 페이지를 새 페이지로 바꿀 수 있음
