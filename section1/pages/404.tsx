83a0fe10f3200bb92c8f37f79a4889656e35a95d
Beanxx <tnqls991211@naver.com>
2022-12-23 23:58:19 +0900

    feat: Custom 404 page 추가

---
+++ section1/pages/404.tsx
@@ -0,0 +1,9 @@
+import styles from "../styles/Home.module.css";
+
+export default function NotFoundPage() {
+  return (
+    <div className={styles.main}>
+      <h1>Page not found!</h1>
+    </div>
+  );
+}
